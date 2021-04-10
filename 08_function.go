016185be8086af9f239504885ff963d8d8a58985
ojwm <21040914+ojwm@users.noreply.github.com>
2021-04-10 17:53:45 +0100

    Function examples

--- 08_function.go
+++ 08_function.go
@@ -15,11 +15,25 @@ func power(value int) (int, int) {
 	return value * value, value * value * value
 }
 
+func do(f func(int) int) int {
+	return f(8)
+}
+
 func main() {
 	names := []string{"Oli", "John", "Terry"}
 	for _, name := range names {
 		fmt.Print(hello(name))
 	}
+	fmt.Println("---------------------------")
 	power2, power3 := power(4)
 	fmt.Println(power2, power3)
+	fmt.Println("---------------------------")
+	invert := func(value int) int {
+		return value * -1
+	}
+	increment := func(value int) int {
+		return value + 1
+	}
+	fmt.Println(do(invert))
+	fmt.Println(do(increment))
 }
